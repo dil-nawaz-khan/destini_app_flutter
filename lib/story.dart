4d97c3c737a5a92ede70a00e73ec5b7d39a752f8
Dilnawaz Khan <dilnawaz@tentwenty.me>
2025-04-20 15:10:02 +0500

    step 4 complete

--- lib/story.dart
+++ lib/story.dart
@@ -2,6 +2,6 @@ class Story {
   late String storyTitle;
   late String choice1;
   late String choice2;
-}
 
-//TODO: Step 4 - Create a Constructor for this class to be able to initialise the properties created in step 3.
+  Story(this.storyTitle, this.choice1, this.choice2);
+}
